72767f296601e7b19588e2b234552b2084bfe0f7
farivar <farivaramiri@gmail.com>
2023-08-12 23:23:18 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    resource :session, only: [:create, :show, :destroy]
+    resources :users, only: [:index, :show, :create, :update, :destroy] 
+    # get '/users/current', to: 'users#current'
+  end
+
 end
