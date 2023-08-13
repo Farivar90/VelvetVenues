97f3ccd9c6fde2af657e83543dd5021411ad3278
farivar <farivaramiri@gmail.com>
2023-08-13 12:07:15 -0700

    handle some errors

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,54 @@
 class Api::UsersController < ApplicationController
-  def create
+
+  wrap_parameters include: User.attribute_names + ['password']
+
+  before_action :require_logged_out, only: [:create, :new]
+
+  before_action :require_logged_in, except: [:create]
+  
+  before_action :set_user, only: [:show, :update, :destroy]
+
+
+  def index
+    @users = User.all
+    render json: @users
   end
+
+  def show
+    render json: @user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :full_name)
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,35 @@
 class ApplicationController < ActionController::API
-        before_action :snake_case_params
+    before_action :snake_case_params, :attach_authenticity_token
+
+    include ActionController::RequestForgeryProtection
+  
+    protect_from_forgery with: :exception
+
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActionController::ParameterMissing, with: :parameter_missing
+    rescue_from StandardError, with: :internal_server_error
+
+    helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: { message: 'Unauthorized' }, status: :unauthorized 
+    end
+  end
+
+  def require_logged_out
+    if logged_in?
+        render json: {errors: ['Must be logged out']}, status: unauthorized 
+    end 
+  end 
+
+  def logged_in?
+    !!current_user
   end
 
   def login!(user)
@@ -14,16 +41,31 @@ class ApplicationController < ActionController::API
     session[:session_token] = nil
     @current_user = nil
   end
-
-  def require_logged_in
-    unless current_user
-      render json: { message: 'Unauthorized' }, status: :unauthorized 
-    end
-  end
-        
+      
     private
 
     def snake_case_params
      params.deep_transform_keys!(&:underscore)
     end
+
+    def record_not_found(exception)
+        render json: { error: "Record not found: #{exception.message}" }, status: :not_found
+    end
+    
+    def parameter_missing(exception)
+        render json: { error: "Parameter missing: #{exception.message}" }, status: :unprocessable_entity
+    end
+    
+    def internal_server_error(exception)
+        render json: { error: "Internal server error: #{exception.message}" }, status: :internal_server_error
+    end
+
+    def invalid(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def attach_authenticity_token
+        headers['X-CSRF-Token'] = masked_authenticity_token(session)
+    end
+    
 end
