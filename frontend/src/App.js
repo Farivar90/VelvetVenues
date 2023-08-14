0ad894cb261e9f450e0920bab4a12e3467cb554e
farivar <farivaramiri@gmail.com>
2023-08-13 22:39:59 -0700

    make header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import RegistrationForm from './store/RegistrationForm';
 import LoginForm from './store/LoginForm';
 import MainPage from './components/MainPage';
 
-
 const App = () => {
   return (
     <Router>
+      <header>
+        <Link to="/">
+          <img src="/resfiles/Logo.png" alt="Logo" />
+        </Link>
+        <div>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      </header>
+
       <Switch>
         <Route path="/register" component={RegistrationForm} />
         <Route path="/login" component={LoginForm} />
