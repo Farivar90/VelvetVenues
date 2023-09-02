197b95c2a3fc5763f1421eba625ba4d6fc66dfe6
farivar <farivaramiri@gmail.com>
2023-09-02 13:40:44 -0700

    search user almost done

--- frontend/src/components/Search/SearchListings.jsx
+++ frontend/src/components/Search/SearchListings.jsx
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams, Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
+import axios from 'axios';
+import { setListings } from  '../../store/listingsReducer';
 
-function SearchListings(){
-    const currentUser = useSelector(state => state.session.user);
-    if (!currentUser) {
-        return <Redirect to={`/`} />;
-      }
-    return(
-        <div>
-            <h1>SearchListings</h1><br/>
-            <h1>Under Construction...</h1>
-        </div>
-    )
-};
+function SearchListings() {
+  const currentUser = useSelector(state => state.session.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const dispatch = useDispatch();
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`/api/listings?search=${searchTerm}`);
+      dispatch(setListings(response.data));
+      setSearchResults(Object.values(response.data));
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
+
+  useEffect(() => {
+    setSearchResults([]);
+    if (searchTerm) {
+      handleSearch();
+    }
+  }, [searchTerm, dispatch]);
+
+  if (!currentUser) {
+    return <Redirect to={`/`} />;
+  }
+
+  return (
+    <div className='search'>
+      <h1 className='search-h1'>Search Listings</h1>
+      <input
+        className='search-input'
+        type="text"
+        value={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+        placeholder="Search by location"
+      />
+      <div className="search-results">
+        {searchResults.map(listing => (
+          <Link to={`/listings/${listing.id}`} key={listing.id} className="listing-box">
+            <img src={listing.imageUrl} alt={`Listing ${listing.id}`} />
+            <div>
+              <p>{listing.location}</p>
+              <p>${listing.price.toLocaleString("en-US")}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default SearchListings;
--- frontend/src/components/Search/SearchUser.jsx
+++ frontend/src/components/Search/SearchUser.jsx
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams, Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
+import './Search.css';
 
-function SearchUser(){
-    const currentUser = useSelector(state => state.session.user);
-    if (!currentUser) {
-        return <Redirect to={`/`} />;
-      }
-    return(
-        <div>
-            <h1>SearchUser</h1><br/>
-            <h1>Under Construction...</h1>
-        </div>
-    )
-};
-export default SearchUser
+function SearchUser() {
+  const currentUser = useSelector(state => state.session.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearchUsername = async () => {
+    try {
+      const response = await fetch(`/api/users?search=${searchTerm}`);
+      const responseData = await response.json();
+      const usersArray = Object.values(responseData);
+
+      setSearchResults(usersArray);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
+
+  const handleSearchFullname = async () => {
+    try {
+      const response = await fetch(`/api/users?full_name=${searchTerm}`);
+      const responseData = await response.json();
+      const usersArray = Object.values(responseData);
+
+      setSearchResults(usersArray);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
+
+  useEffect(() => {
+    setSearchResults([]);
+    if (searchTerm) {
+      handleSearchUsername();
+      handleSearchFullname();
+    }
+  }, [searchTerm]);
+
+  if (!currentUser) {
+    return <Redirect to={`/`} />;
+  }
+
+  return (
+    <div className="search">
+      <h1 className="search-h1" >Search by username or full name</h1>
+      <input className='search-input'
+        type="text"
+        value={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+        placeholder="Search by username or full name"
+      />
+      <div className="user-profile-boxes">
+        {/* <h2>Results:</h2> */}
+        {searchResults.map(user => (
+          <Link to={`/users/${user.id}`} key={user.id} className="profile-box">
+            <img src={user.imageUrl} alt={`Profile of ${user.username}`} />
+            <div>
+              <p>{user.username}</p>
+              <p>{user.fullName}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SearchUser;
