3263f34eb6ad4c3642cf98b5313b217fbb120cd8
farivar <farivaramiri@gmail.com>
2023-09-05 11:57:36 -0700

    fucked up

--- app/controllers/api/v1/chatbot_controller.rb
+++ app/controllers/api/v1/chatbot_controller.rb
@@ -1,6 +1,9 @@
 # app/controllers/api/v1/chatbot_controller.rb
 
 class Api::V1::ChatbotController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
     def chat
       response = Openai::Completion.create(
         model: "text-davinci-002",
