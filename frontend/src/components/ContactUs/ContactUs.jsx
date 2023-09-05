8cd4f6d56cae9b6f337f544cea0c9823718df162
farivar <farivaramiri@gmail.com>
2023-09-04 17:26:54 -0700

    footer done

---
+++ frontend/src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactUs(){
+
+    return(
+        <div>
+            <h1>About Project</h1><br/>
+            <h1>Under Construction...</h1>
+        </div>
+    )
+};
+export default ContactUs;
